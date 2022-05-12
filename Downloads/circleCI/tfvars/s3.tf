16111430f14a59b9afca6e6805cecc418ecc479f
Shantanu Srivastava <shantanu.s04@ad.infosys.com>
2022-05-12 16:29:48 +0530

    repo added

---
+++ Downloads/circleCI/tfvars/s3.tf
@@ -0,0 +1,11 @@
+resource "aws_s3_bucket" "onebucket" {
+   bucket = "sample s3"
+   acl = "private"
+   versioning {
+      enabled = true
+   }
+   tags = {
+     Name = "Bucket1"
+     Environment = "Test"
+   }
+}
