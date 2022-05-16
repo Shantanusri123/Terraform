88d32a55e7e714f9772316600878cd96e20a8283
Shantanusri123 <105036664+Shantanusri123@users.noreply.github.com>
2022-05-16 16:00:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,4 @@
 resource "aws_s3_bucket" "onebucket" {
-   bucket = "sample-circleci-s3"
-   acl = "private"
-   versioning {
-      enabled = true
-   }
-   tags = {
-     Name = "sample-circleci-s3"
-     Environment = "Test"
-   }
+  bucket = var.bucket
+   acl = var.acl
 }
