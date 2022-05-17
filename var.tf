344df81f6f96110b2b53cdf6c944e11105ac51b0
Shantanusri123 <105036664+Shantanusri123@users.noreply.github.com>
2022-05-17 18:46:35 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,9 +1,9 @@
-variable "bucket_name" {
+variable "bucket_name1" {
  type = string
  default = "s3_test"
 }
 
-variable "acl_value" {
+variable "acl_value1" {
  type = string
  default = "private"
 }
