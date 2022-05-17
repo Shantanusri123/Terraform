6f13359a0239e7d666dc25017ec291718b742e3d
Shantanusri123 <105036664+Shantanusri123@users.noreply.github.com>
2022-05-17 18:51:04 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -9,13 +9,3 @@ default = "rBPUUjvGAwYMtKobXEu+jj2ihfzMXEo4A112PgUD"
 variable "aws_region" {
 default = "ap-south-1"
 }
-
-variable "bucket_name" {
- type = string
- default = "s3_test"
-}
-
-variable "acl_value" {
- type = string
- default = "private"
-}
