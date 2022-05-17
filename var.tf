c037f1c90c3711322a930536e90e61dbcdd94210
Shantanu Srivastava <shantanu.s04@ad.infosys.com>
2022-05-17 18:01:50 +0530

    Added the files

--- var.tf
+++ var.tf
@@ -1,17 +1,9 @@
 #AWS Config
 
-variable "aws_access_key" {
-default = "AKIAWMPJTW2YUE6S5MFN"
-}
-variable "aws_secret_key" {
-default = "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"
-}
-variable "aws_region" {
-default = "ap-south-1"
-}
 variable "bucket" {
   type = string
 }
+
 variable "acl" {
   type = string
   default = "private"
