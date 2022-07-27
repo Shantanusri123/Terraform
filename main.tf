5f630a0ec5ac194dc9e7f459b31c8b1f8946d874
Shantanusri123 <105036664+Shantanusri123@users.noreply.github.com>
2022-07-27 17:11:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
-resource "aws_s3_bucket" "onebucket" {
-  bucket = var.bucket_name 
-  acl = var.acl_value
-}
+# resource "aws_s3_bucket" "onebucket" {
+#   bucket = var.bucket_name 
+#   acl = var.acl_value
+# }
+
+resource "aws_instance" "ec2instance"{
+  ami = var.ami
+  instnace_type = t2.micro
+  security_groups = [aws_security_group.
