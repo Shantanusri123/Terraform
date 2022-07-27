cf8723c22c1f5d8d86460bab44547396955cb43d
Shantanusri123 <105036664+Shantanusri123@users.noreply.github.com>
2022-07-27 17:13:42 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,10 +1,19 @@
-variable "bucket_name" {
+# variable "bucket_name" {
+#  type = string
+# }
+
+# variable "acl_value" {
+#  type = string
+# }
+
+# variable "aws_region" {
+# }
+ variable "ami"{
+  type = string
+ }
+
+var "name"{
  type = string
 }
 
-variable "acl_value" {
- type = string
-}
 
-variable "aws_region" {
-}
